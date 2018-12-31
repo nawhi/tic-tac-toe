fd98fe82285b4c8fc51da501142a0376376d1a9b
nawhi <25394965+nawhi@users.noreply.github.com>
2018-12-31 15:19:22 +0000

    Don't allow same player to go twice

--- src/main/java/tictactoe/TicTacToeGame.java
+++ src/main/java/tictactoe/TicTacToeGame.java
@@ -1,12 +1,29 @@
 package tictactoe;
 
 public class TicTacToeGame {
+
+    private class WhoseTurn {
+        private boolean isPlayerX = true;
+
+        private void next() {
+            isPlayerX = !isPlayerX;
+        }
+
+        public boolean ofPlayerX() {
+            return isPlayerX;
+        }
+    }
+    private WhoseTurn turn = new WhoseTurn();
+
     public GameState state() {
         return null;
     }
 
     public void playerX(RowIndex row, ColumnIndex column) {
+        if (!turn.ofPlayerX())
+            throw new OrderOfPlayException("It's not Player X's turn");
 
+        turn.next();
     }
 
     public void playerO(RowIndex row, ColumnIndex column) {
--- src/test/java/tictactoe/TicTacToeGameShould.java
+++ src/test/java/tictactoe/TicTacToeGameShould.java
@@ -1,16 +1,31 @@
 package tictactoe;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TicTacToeGameShould {
 
+    private TicTacToeGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new TicTacToeGame();
+    }
+
     @Test
     public void not_allow_player_O_to_go_first() {
-        var game = new TicTacToeGame();
         assertThrows(OrderOfPlayException.class,
                 () -> game.playerO(new RowIndex(0), new ColumnIndex(0)));
+    }
+
+    @Test
+    public void not_allow_same_player_to_go_twice() {
+        game.playerX(new RowIndex(0), new ColumnIndex(0));
+
+        assertThrows(OrderOfPlayException.class,
+                () -> game.playerX(new RowIndex(0), new ColumnIndex(1)));
     }
 
 }
