f511663fd4f0648e5fb1cdbe65e524dc8b4aa0b0
nawhi <25394965+nawhi@users.noreply.github.com>
2019-01-02 09:18:07 +0000

    Add new AT for players winning

--- src/main/java/tictactoe/GameState.java
+++ src/main/java/tictactoe/GameState.java
@@ -3,6 +3,7 @@ package tictactoe;
 public enum GameState {
     INITIAL,
     IN_PROGRESS,
+    PLAYER_X_WINS,
     DRAW
 
 }
--- src/test/java/AT_TicTacToe.java
+++ src/test/java/AT_TicTacToe.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import tictactoe.ColumnIndex;
 import tictactoe.GameState;
 import tictactoe.RowIndex;
@@ -8,6 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AT_TicTacToe {
 
+    public static final RowIndex ROW_0 = new RowIndex(0);
+    public static final RowIndex ROW_1 = new RowIndex(1);
+    public static final RowIndex ROW_2 = new RowIndex(2);
+    public static final ColumnIndex COLUMN_0 = new ColumnIndex(0);
+    public static final ColumnIndex COLUMN_1 = new ColumnIndex(1);
+    public static final ColumnIndex COLUMN_2 = new ColumnIndex(2);
+
+
+    private Game game;
+
     /*
     initial acceptance:
     - a game has 9 fields in a 3x3 grid
@@ -17,10 +28,13 @@ public class AT_TicTacToe {
     then: early winner
      */
 
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     public void game_is_draw_if_all_fields_are_occupied() {
-        Game game = new Game();
-
         /*
          X │ X │ O
         ───┼───┼───
@@ -28,16 +42,34 @@ public class AT_TicTacToe {
         ───┼───┼───
          X │ O │ O
          */
-        game.playerX(new RowIndex(1), new ColumnIndex(0));
-        game.playerO(new RowIndex(0), new ColumnIndex(1));
-        game.playerX(new RowIndex(1), new ColumnIndex(1));
-        game.playerO(new RowIndex(1), new ColumnIndex(2));
-        game.playerX(new RowIndex(0), new ColumnIndex(0));
-        game.playerO(new RowIndex(2), new ColumnIndex(2));
-        game.playerX(new RowIndex(0), new ColumnIndex(2));
-        game.playerO(new RowIndex(2), new ColumnIndex(0));
-        game.playerX(new RowIndex(2), new ColumnIndex(1));
+        game.playerX(ROW_1, COLUMN_0);
+        game.playerO(ROW_0, COLUMN_1);
+        game.playerX(ROW_1, COLUMN_1);
+        game.playerO(ROW_1, COLUMN_2);
+        game.playerX(ROW_0, COLUMN_0);
+        game.playerO(ROW_2, COLUMN_2);
+        game.playerX(ROW_0, COLUMN_2);
+        game.playerO(ROW_2, COLUMN_0);
+        game.playerX(ROW_2, COLUMN_1);
 
         assertEquals(GameState.DRAW, game.state());
+    }
+
+    @Test
+    public void X_wins_if_they_have_three_in_a_row() {
+        /*
+         X │ O │ O
+        ───┼───┼───
+         X │   │
+        ───┼───┼───
+         X │   │
+         */
+        game.playerX(ROW_0, COLUMN_0);
+        game.playerO(ROW_0, COLUMN_1);
+        game.playerX(ROW_1, COLUMN_0);
+        game.playerO(ROW_0, COLUMN_2);
+        game.playerX(ROW_2, COLUMN_0);
+
+        assertEquals(GameState.PLAYER_X_WINS, game.state());
     }
 }
