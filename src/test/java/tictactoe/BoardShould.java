8e52e10baa90469fefc1650d8e2d36b0706d49fa
nawhi <25394965+nawhi@users.noreply.github.com>
2019-01-02 11:41:16 +0000

    Use SquareIndex in BoardShould

--- src/test/java/tictactoe/BoardShould.java
+++ src/test/java/tictactoe/BoardShould.java
@@ -7,13 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class BoardShould {
-
-    public static final RowIndex ROW_0 = new RowIndex(0);
-    public static final RowIndex ROW_1 = new RowIndex(1);
-    public static final RowIndex ROW_2 = new RowIndex(2);
-    public static final ColumnIndex COLUMN_0 = new ColumnIndex(0);
-    public static final ColumnIndex COLUMN_1 = new ColumnIndex(1);
-    public static final ColumnIndex COLUMN_2 = new ColumnIndex(2);
     private Board board;
 
     @BeforeEach
@@ -28,39 +21,40 @@ class BoardShould {
 
     @Test
     public void have_IN_PROGRESS_state_when_half_full() {
-        board.addX(new RowIndex(0), new ColumnIndex(0));
+        board.addX(new SquareIndex(0, 0));
         assertEquals(GameState.IN_PROGRESS, board.state());
     }
 
     @Test
     public void have_DRAW_state_when_filled() {
-        board.addX(new RowIndex(1), new ColumnIndex(0));
-        board.addO(new RowIndex(0), new ColumnIndex(1));
-        board.addX(new RowIndex(1), new ColumnIndex(1));
-        board.addO(new RowIndex(1), new ColumnIndex(2));
-        board.addX(new RowIndex(0), new ColumnIndex(0));
-        board.addO(new RowIndex(2), new ColumnIndex(2));
-        board.addX(new RowIndex(0), new ColumnIndex(2));
-        board.addO(new RowIndex(2), new ColumnIndex(0));
-        board.addX(new RowIndex(2), new ColumnIndex(1));
+        board.addX(new SquareIndex(1, 0));
+        board.addO(new SquareIndex(0, 1));
+        board.addX(new SquareIndex(1, 1));
+        board.addO(new SquareIndex(1, 2));
+        board.addX(new SquareIndex(0, 0));
+        board.addO(new SquareIndex(2, 2));
+        board.addX(new SquareIndex(0, 2));
+        board.addO(new SquareIndex(2, 0));
+        board.addX(new SquareIndex(2, 1));
 
         assertEquals(GameState.DRAW, board.state());
     }
 
     @Test
     public void have_PLAYER_X_WINS_state_when_X_has_horizontal_three() {
-        board.addX(ROW_0, COLUMN_0);
-        board.addX(ROW_0, COLUMN_1);
-        board.addX(ROW_0, COLUMN_2);
+        board.addX(new SquareIndex(0, 0));
+        board.addX(new SquareIndex(0, 1));
+        board.addX(new SquareIndex(0, 2));
 
         assertEquals(GameState.PLAYER_X_WINS, board.state());
     }
 
     @Test
     public void prevent_adding_to_occupied_squares() {
-        board.addX(new RowIndex(0), new ColumnIndex(0));
+        SquareIndex index = new SquareIndex(0, 0);
+        board.addX(index);
         assertThrows(OccupiedSpaceException.class,
-                () -> board.addO(ROW_0, COLUMN_0));
+                () -> board.addO(index));
     }
 
 }
